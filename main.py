e7832e64fdb202acb5dc52ccbe54ba73b199e0f2
gamename <tennis.n.smith@gmail.com>
2023-09-02 13:25:09 -0500

    refactor for clarity

--- main.py
+++ main.py
@@ -14,40 +14,24 @@ from machine import Pin
 
 import secrets
 
-MAX_HOSTNAME_LENGTH = 15  # The API only allows a max of 15 chars (grr)
 NETWORK_SLEEP_INTERVAL = 10  # seconds
+MINUTES = 10
+CONTACT_PIN = 0
+PAUSE_MINUTES = 60 * MINUTES
 
 
-def set_picow_hostname(name):
-    """
-    Set the hostname of the Raspberry Pi Pico W
-
-    Args:
-        name - a string
-
-    Returns:
-        Nothing
-    """
-    if len(name) > MAX_HOSTNAME_LENGTH:
-        # current api only allows a max of 15 chars
-        new_name = name[:MAX_HOSTNAME_LENGTH]
-    else:
-        new_name = name
-    network.hostname(new_name)
-    return
-
-
-def connect():
+def connect(hostname):
     """
     Connect to Wi-Fi
 
-    Args: None
+    Args: hostname
 
     Returns:
         True when successful
     """
     led = Pin("LED", Pin.OUT)
     led.off()
+    network.hostname(hostname)
     wlan = network.WLAN(network.STA_IF)
     wlan.config(pm=0xa11140)  # turn OFF power save mode
     wlan.active(True)
@@ -59,18 +43,17 @@ def connect():
     return True
 
 
-MINUTES = 10
-CONTACT_PIN = 0
-PAUSE_MINUTES = 60 * MINUTES
-contact_switch = Pin(CONTACT_PIN, Pin.IN, Pin.PULL_UP)
+def main():
+    contact_switch = Pin(CONTACT_PIN, Pin.IN, Pin.PULL_UP)
 
-set_picow_hostname('pico-garage')
+    if connect(secrets.HOSTNAME):
+        while True:
+            # if connection is broken, the garage door is open
+            if not contact_switch.value():
+                # Tell the REST API so we get a notification
+                requests.post(secrets.URL, headers={'content-type': 'application/json'})
+                time.sleep(PAUSE_MINUTES)
 
-if connect():
-    while True:
-        # if connection is broken, the garage door is open
-        if not contact_switch.value():
-            # Tell the REST API so we get a notification
-            requests.post(secrets.URL,
-                          headers={'content-type': 'application/json'})
-            time.sleep(PAUSE_MINUTES)
+
+if __name__ == "__main__":
+    main()
