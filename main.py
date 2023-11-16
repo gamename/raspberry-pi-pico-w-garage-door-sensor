e5e3915e39612545ab62eadbb944e6a5a41b9477
gamename <tennis.n.smith@gmail.com>
2023-11-16 16:52:13 -0600

    take out ota

--- main.py
+++ main.py
@@ -26,14 +26,6 @@ CONTACT_PIN = 22  # GPIO pin #22, physical pin #29
 REQUEST_HEADER = {'content-type': 'application/json'}
 
 #
-# Files we want to update over-the-air (OTA)
-OTA_UPDATE_GITHUB_REPOS = {
-    "gamename/raspberry-pi-pico-w-garage-door-sensor": ["boot.py", "main.py"],
-    "gamename/micropython-over-the-air-utility": ["ota.py"],
-    "gamename/micropython-gamename-utilities": ["utils.py", "cleanup_logs.py"]
-}
-
-#
 # How long should we pause before rechecking status?
 DOOR_OPEN_PAUSE_TIMER = 600  # seconds (10 min)
 
@@ -57,14 +49,6 @@ def main():
     utils.tprint("MAIN: Handle any old traceback logs")
     utils.purge_old_log_files()
     #
-    utils.tprint("MAIN: Configure OTA.")
-    ota_updater = OTAUpdater(secrets.GITHUB_USER,
-                             secrets.GITHUB_TOKEN,
-                             OTA_UPDATE_GITHUB_REPOS,
-                             update_interval_minutes=1440,
-                             update_on_initialization=True,
-                             debug=DEBUG)
-
     reed_switch = Pin(CONTACT_PIN, Pin.IN, Pin.PULL_DOWN)
 
     utils.tprint("MAIN: Starting event loop")
@@ -76,8 +60,6 @@ def main():
             resp = requests.post(secrets.REST_API_URL, headers=REQUEST_HEADER)
             resp.close()
             time.sleep(DOOR_OPEN_PAUSE_TIMER)  # 10 min
-        else:
-            ota_updater.updated()
 
         if not wlan.isconnected():
             utils.tprint("MAIN: Restart network connection.")
